97a745a27f80583eab60516743aa9ae1811b7e9d
kyle <belfortk@gmail.com>
2018-01-25 18:58:53 -0800

    initial commit

---
+++ mortgage-calculator.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Mortgage Calculator
+Plugin URI: https://fullbeaker.com/kylebelfort-application
+Description: WP plugin to determine monthly mortgage payments from a form.
+Author: Kyle Belfort
+Version: 1.0
+Author URI: https://kylebelfort.com/
+*/
